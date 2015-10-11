f867b5f8f962b505a7813cc83d34df27343e0cff
josepamar <jpachemar@gmail.com>
2015-10-11 17:03:02 +0200

    Homework_Module_4

--- RestaurantManager/OrderPage.xaml.cs
+++ RestaurantManager/OrderPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using restaurantManager.Models;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -30,6 +31,13 @@ namespace RestaurantManager
         private void home_click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MainPage), null);
+        }
+
+        private void addOrder(object sender, RoutedEventArgs e)
+        {
+            string text = listView.SelectedItem.ToString();
+            dataManager addOrder = new dataManager();
+            addOrder.CurrentlySelectedMenuItems.Add(text);
         }
     }
 }
